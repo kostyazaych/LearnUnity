5422be4364e267abfaf4ad2b7134425e437f20ec
kostyazaych <kostya.from.rostov@gmail.com>
2020-02-26 20:08:11 +0300

    Управление

--- Assets/Scripts/Character/Input/MovementInput.cs
+++ Assets/Scripts/Character/Input/MovementInput.cs
@@ -16,10 +16,15 @@ public class MovementInput : MonoBehaviour
     public Camera PlayerCamera;
     private Vector3 TestVelocity;
     private Transform mainCameraTransform = null;
+
+    public float mouseSense = 0.1f;
+    [SerializeField] private Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-           controller = GetComponent<CharacterController>();
+        Cursor.visible = true;
+        controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         GetComponent<PlayerCharacter>().PlayerCamera = PlayerCamera;
         mainCameraTransform = PlayerCamera.transform;
@@ -29,14 +34,19 @@ public class MovementInput : MonoBehaviour
     void Update()
     {
          Move();
+         RotateCharacter();
     }
 
 
     private void Move() {
         Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
         
-        Vector3 forward = mainCameraTransform.forward;
-        Vector3 right = mainCameraTransform.right;
+        /*Vector3 forward = mainCameraTransform.forward;
+        Vector3 right = mainCameraTransform.right;*/
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
 
         forward.y = 0;
         right.y = 0;
@@ -44,9 +54,11 @@ public class MovementInput : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
-       
-        
-        Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
+
+
+         Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
+
         Vector3 gravityVector = Vector3.zero;
 
         if (!controller.isGrounded)
@@ -64,10 +76,83 @@ public class MovementInput : MonoBehaviour
 
         controller.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
         controller.Move(gravityVector * Time.deltaTime);
+        
+
 
         //animator.SetFloat("Speed", 0.5f * movementInput.magnitude, speedSmoothTime, Time.deltaTime);
         animator.SetFloat("Speed", movementInput.magnitude);
          TestVelocity = desiredMoveDirection * currentSpeed * Time.deltaTime;
         //Debug.Log(movementInput.magnitude);
     }
+
+    void RotateCharacter() {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSense;
+        float rotAmountY = mouseY * mouseSense;
+
+        Vector3 rotPlayer = player.transform.rotation.eulerAngles;
+
+       // rotPlayer.x -= rotAmountY;
+        rotPlayer.z = 0;
+        rotPlayer.x = 0;
+
+        rotPlayer.y += rotAmountX;
+
+
+        player.rotation = Quaternion.Euler(rotPlayer);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public GameObject[] structuresTypes;
+Vector3 mousePos;
+GameObject _structureTypes;
+GameObject pre_structure;
+bool isBuilding = false;
+
+
+void Update()
+{
+    float x = Input.mousePosition.x;
+    float z = Input.mousePosition.z;
+    float y = 0.25f;
+    mousePos = new Vector3(x, y, z);
+
+    if (isBuilding == true)
+    {
+        pre_structure.transform.position = mousePos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(pre_structure);
+            Instantiate(_structureTypes, mousePos, Quaternion.identity);
+            isBuilding = false;
+        }
+    }
+}
+
+
+public void SelectFarm()
+{
+    _structureTypes = structuresTypes[0];
+    isBuilding = true;
+    Destroy(pre_structure);
+    pre_structure = (GameObject)Instantiate(_structureTypes, mousePos, Quaternion.identity);
+}
+ 
+ 
+ }
