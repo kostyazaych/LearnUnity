15867fb0b7f9afaccdc93c9413cc4367767af60b
kostyazaych <kostya.from.rostov@gmail.com>
2020-03-03 18:18:12 +0300

    Новый меш

---
+++ Assets/Scripts/Weapons/Pistols/Pistol1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol1 : SingleShotBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        base.Start();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override public void BaseAttack() {
+        AttackSingleShotBase();
+    }
+}
---
+++ Assets/Scripts/Weapons/Pistols/SingleShotBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleShotBase : WeaponBase
+{
+
+    private int fullAmmoAmount;
+    private int ammoLeftInMag;
+
+    // Start is called before the first frame update
+     public virtual void Start()
+    {
+        StartSingleShotBase();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartSingleShotBase() {
+       
+    }
+
+   protected void AttackSingleShotBase() {
+        Debug.Log("AttackSingleShotBase");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
